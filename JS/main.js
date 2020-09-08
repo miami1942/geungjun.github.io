2b08534f6b09bba17d954d7486aafce4acbde682
miami1942 <wkdgmd7386@gmail.com>
2020-09-08 23:40:33 +0900

    다음에 뭐 추가하지?

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,35 @@
+var list = $("#header #gnb");
+
+list.on("mouseenter", function () {
+    $("#header").addClass("on");
+});
+list.on("mouseleave", function () {
+    $("#header").removeClass("on");
+})
+
+var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+})
+
 new fullpage('#fullpage', {
     navigation: true,
-    slidesNavigation: true,
     scrollingSpeed: 500,
-    onLeave: function (origin, destination, direction) {
-        console.log("Leaving section" + origin.index);
-    },
-    resetSliders: true,
-    touchSensitivity: 15,
+    scrollBar: true,
 });
