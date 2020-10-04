a3380be31199cf5351505db157ae3cab3575225c
miami1942 <wkdgmd7386@gmail.com>
2020-10-04 17:43:33 +0900

    위로 버튼 추가, 메뉴수정, 글좀 수정하면 끝날듯

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
 var state = false;
-var list = $("#header #gnb > ul").height()
-let slide_switch = 1
-let slide_time = 5
+var list = $("#header #gnb > ul").height();
+let slide_switch = 1;
+//let up_switch = 1;
 
 function slide() {
     $(".slide_sign").animate({
@@ -20,6 +20,23 @@ function slide() {
                     $(this).fadeOut(1000)
                     //}, 4000);
                 })
+            })
+        })
+    })
+}
+function up() {
+    $("#up_img").animate({
+        marginTop: "-1.5rem",
+    }, 700, function () {
+        $(this).animate({
+            marginTop: "0rem",
+        }, 700, function () {
+            $(this).animate({
+                marginTop: "-1.5rem",
+            }, 700, function () {
+                $(this).animate({
+                    marginTop: "0rem",
+                }, 700)
             })
         })
     })
@@ -48,10 +65,9 @@ var mySwiper = new Swiper('.swiper-container', {
 
 new fullpage('#fullpage', {
     navigation: true,
-    navigationTooltips: ['STRAT', '목차', '자기소개-인적',
+    navigationTooltips: ['start', '목차', '자기소개-인적',
         '자기소개-학력', '자기소개-기술력', '프로젝트1', '프로젝트2',
-        '프로젝트3', '프로젝트4', '위로', '기타'
-    ],
+        '프로젝트3', '위로가기', '작성자'],
     scrollingSpeed: 500,
     scrollBar: false,
     onLeave: function (origin, destination, direction) {
@@ -64,6 +80,15 @@ new fullpage('#fullpage', {
                 }, 1200);
 
             }
+        }
+        if (destination.index === 8) {
+            //if (up_switch === 1) {
+                up_switch = 0
+                setTimeout(function () {
+                    up()
+                }, 300);
+
+            //}
         }
     },
 });
@@ -81,10 +106,19 @@ $("#expand_header").click(function () {
     //$("#header").toggleClass("on");
 })
 $("#fullpage").on("mouseenter", function () {
-    setTimeout(function () {
-        $("#header").height(0);
-        $("#expand_header").removeClass("on");
-        state = false;
-        //$("#header").removeClass("on")
-    }, 0)
+    $("#header").height(0);
+    $("#expand_header").removeClass("on");
+    state = false;
+    //$("#header").removeClass("on")
+});
+$("#fullpage").on("click", function () {
+    $("#header").height(0);
+    $("#expand_header").removeClass("on");
+    state = false;
+    //$("#header").removeClass("on")
+});
+
+$("#up_box").on('click', function () {
+    console.log("up click")
+    fullpage_api.moveTo(1, 0);
 });
